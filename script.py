ae80e242bb289bd303108cefdf17e900e24a95ba
Jaime Nufio <jaimeenufio@gmail.com>
2020-11-09 22:39:29 -0500

    testing with removed/privated videos

--- script.py
+++ script.py
@@ -53,8 +53,8 @@ def scanPlaylist(id):
         except:
             currentPlaylists[name][i]={}
             currentPlaylists[name][i]['Title'] = "Failed to Retrieve"
-            currentPlaylists[name][videoData['id']]['VideoUrl'] = "" #"https://www.youtube.com/watch?v="+videoData['id']
-            currentPlaylists[name][videoData['id']]['ChannelUrl'] = "" #"https://www.youtube.com/channel/"+videoData['snippet']['channelId']
+            currentPlaylists[name][i]['VideoUrl'] = "" #"https://www.youtube.com/watch?v="+videoData['id']
+            currentPlaylists[name][i]['ChannelUrl'] = "" #"https://www.youtube.com/channel/"+videoData['snippet']['channelId']
 
 def comparePlaylist(id):
 
@@ -81,6 +81,7 @@ def comparePlaylist(id):
             if song not in currentPlaylists[id] and song != "Failed to Retrieve":
                 #print("\""+storedPlaylist[id][song]['Title']+"\" is missing.")
                 missing[song]={}
+                #print(storedPlaylist[id][song])
                 missing[song]['Title'] = storedPlaylist[id][song]['Title']
                 missing[song]['VideoUrl'] = storedPlaylist[id][song]['VideoUrl']
                 missing[song]['ChannelUrl'] = storedPlaylist[id][song]['ChannelUrl']
@@ -102,6 +103,10 @@ def comparePlaylist(id):
         with open("./json/playlists.json", "w") as file:
             for song in toAdd:
                 storedPlaylist[song] = toAdd[song]
+                if 'ChannelUrl' not in toAdd[song].keys():
+                    toAdd[song]['ChannelUrl'] = "NULL"
+                if 'VideoUrl' not in toAdd[song].keys():
+                    toAdd[song]['VideoUrl'] = "NULL"
                 #print(currentPlaylists[id][song]['Title'],"added to database.")
             json.dump(storedPlaylist,file,indent=4)
         
@@ -110,22 +115,45 @@ def comparePlaylist(id):
     diffText+="\nUpdates for "+name+":\n"
     if len(missing)+len(toAdd) != 0:                
         for item in missing:
+            if (missing[item]['Title'] == "Failed to Retrieve"):
+                continue
+
             print("-",missing[item]['Title'],"From",missing[item]['ChannelUrl'])
-            diffText+="- "+missing[item]['Title']+" From"+missing[item]['ChannelUrl']+"\n"
+            diffText+="- "+missing[item]['Title']+" From "+missing[item]['ChannelUrl']+"\n"
         for item in toAdd:
-            print("+",toAdd[item]['Title'],"From",toAdd[item]['ChannelUrl'])
+            if (toAdd[item]['Title'] == "Failed to Retrieve"):
+                continue
+
+            print("+",toAdd[item]['Title'],"From ",toAdd[item]['ChannelUrl'])
             diffText+="+ "+toAdd[item]['Title']+" From "+toAdd[item]['ChannelUrl']+"\n"
     else:
         print("No changes.")
         diffText+="No changes.\n"
         
 def writeCurrentPlaylist():
+
+    toWrite = {}
+
+    #Remove absent data on rewriting database.
+    for playlist in currentPlaylists.keys():
+        #print(playlist)
+        toWrite[playlist] = {} 
+        for song in currentPlaylists[playlist]:
+            #print(currentPlaylists[item][song])
+            if not (currentPlaylists[playlist][song]['Title'] == 'Failed to Retrieve'):
+              # del to[item][song]
+              #print("Remove",currentPlaylists[item][song]['Title'],"?")
+                toWrite[playlist][song] = currentPlaylists[playlist][song]
+            else:
+                print(currentPlaylists[playlist][song])
+
     with open("./json/playlists.json", "w") as file:
-        json.dump(currentPlaylists,file,indent=4)
+        json.dump(toWrite,file,indent=4)
 
     data = {}
     with open('./json/config.json','r') as file:
         data = json.load(file)
+
     data['date'] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     with open('./json/config.json','w') as file:
         json.dump(data,file,indent=4)
